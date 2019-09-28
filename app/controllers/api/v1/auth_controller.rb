b2d1e790bfe155533ebef8b9a693d18ccbda0641
saazim1123 <saazim1123@gmail.com>
2019-09-28 22:44:15 +0100

    Auth_controller set up

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::AuthController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: user
+        else
+            render json: {error: 'User is invalid'}, status: 401
+        end
+    end
+
+
+    def show
+
+    token = request.headers['Authorization']
+    user = User.find_by(id: token)
+     if user
+      render json: user
+     else
+      render({json: {error: 'Invalid token'}, status: 401})
+     end
+
+    end
+
+end
