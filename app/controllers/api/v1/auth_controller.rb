9d3fac8ef2558cd91a533db78eca1ced10c8ad71
saazim1123 <saazim1123@gmail.com>
2019-10-02 11:52:07 +0100

    Backend working

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::AuthController < ApplicationController
 
     def create
-        user = User.find_by(email: user_login_params[:email])
-        if user && user.authenticate(user_login_params[:password])
-            render json: { user: UserSerializer.new(user), token: encode_token(user_id: user.id) }, status: :accepted
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: { user: user.username, token: encode_token({id: user.id}) }, status: :accepted
         else
             render json: { message: 'Invalid email or password' }, status: :unauthorized
         end
@@ -31,6 +31,12 @@ class Api::V1::AuthController < ApplicationController
     #   render({json: {error: 'Invalid token'}, status: 401})
     #  end
 
+    end
+
+    private
+
+    def user_login_params
+        params.require(:user).permit(:username, :password)
     end
 
 end
